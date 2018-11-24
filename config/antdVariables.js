980eaa99379fcebe3f680878ba91104963501590
EugeneDz <e.dzhevadov@gmail.com>
2018-11-24 21:03:36 +0200

    feat(theme): add default theme colors

--- config/antdVariables.js
+++ config/antdVariables.js
@@ -1,5 +1,8 @@
 module.exports = {
-  '@primary-color': '#D14D4B',
-  '@link-color': '#D14D4B',
+  '@primary-color': '#6173AA',
+  '@link-color': '#6173AA',
+  '@success-color': '#72C197',
+  '@warning-color': '#FCC86C',
+  '@error-color': '#D14D4B',
   '@border-radius-base': '2px'
 };
