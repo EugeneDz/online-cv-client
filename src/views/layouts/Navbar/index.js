d770c073b7122182088e31cc7e4243aaa52ec720
EugeneDz <e.dzhevadov@gmail.com>
2018-12-10 20:32:44 +0200

    feat(navbar): add logout functionality

--- src/views/layouts/Navbar/index.js
+++ src/views/layouts/Navbar/index.js
@@ -1,25 +1,78 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { Layout, Menu, Avatar, Icon } from 'antd';
+
+import { unsetCurrentUser } from '../../../store/actions/auth';
 
 const { Header } = Layout;
+const { SubMenu } = Menu;
 
-const Navbar = () => (
-  <Header style={{ background: '#fff', padding: 0 }}>
-    <Menu
-      theme="light"
-      mode="horizontal"
-      defaultSelectedKeys={['1']}
-      style={{ lineHeight: '64px', textAlign: 'right' }}
-    >
-      <Menu.Item key="1">
-        <Link to="/sign-up">Sign Up</Link>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Link to="/sign-in">Sign In</Link>
-      </Menu.Item>
-    </Menu>
-  </Header>
-);
+const Navbar = ({
+  unsetCurrentUser: _unsetCurrentUser,
+  auth: { isAuthenticated, user },
+  history
+}) => {
+  const userTitle = () => (
+    <span className="submenu-title-wrapper">
+      <Avatar src={user.avatar} /> {user.name}
+    </span>
+  );
 
-export default Navbar;
+  const logout = () => {
+    localStorage.clear();
+    _unsetCurrentUser();
+    history.push('/sign-in');
+  };
+
+  return (
+    <Header style={{ background: '#fff', padding: 0 }}>
+      {isAuthenticated ? (
+        <Menu
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ lineHeight: '64px', textAlign: 'right' }}
+        >
+          <SubMenu key="sub1" title={userTitle()}>
+            <Menu.Item key="setting:1">
+              <Link to={`/profile/${user.id}`}>
+                <Icon type="idcard" /> Profile
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="setting:2" onClick={logout}>
+              <Icon type="logout" /> Logout
+            </Menu.Item>
+          </SubMenu>
+        </Menu>
+      ) : (
+        <Menu
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px', textAlign: 'right' }}
+        >
+          <Menu.Item key="1">
+            <Link to="/sign-up">Sign Up</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/sign-in">Sign In</Link>
+          </Menu.Item>
+        </Menu>
+      )}
+    </Header>
+  );
+};
+
+const mapStateToProps = ({ auth }) => ({
+  auth
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    unsetCurrentUser
+  }
+)(withRouter(Navbar));
