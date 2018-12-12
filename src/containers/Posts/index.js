e889b3327a5ef7dc58e814d673546219282fd3db
EugeneDz <e.dzhevadov@gmail.com>
2018-12-12 21:08:47 +0200

    feat(posts): add styled section

--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import { Section } from './styled-components';
+
 const Posts = () => (
   <>
     <Helmet>
       <title>Online CV | Posts</title>
       <meta name="description" content="Online CV for developers" />
     </Helmet>
-    <section style={{ minHeight: '100vh', background: '#fff', padding: 24, margin: 24 }}>
+    <Section>
       <h1>Posts Page</h1>
-    </section>
+    </Section>
   </>
 );
 
