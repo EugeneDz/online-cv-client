11184b05701250b1307ebf5ee12c6d75f900b4d0
EugeneDz <e.dzhevadov@gmail.com>
2018-12-12 21:10:11 +0200

    feat(dashboard): add styled section

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import { Section } from './styled-components';
+
 const Dashboard = () => (
   <>
     <Helmet>
       <title>Online CV | Dashboard</title>
       <meta name="description" content="Online CV for developers" />
     </Helmet>
-    <section style={{ minHeight: '100vh', background: '#fff', padding: 24, margin: 24 }}>
+    <Section>
       <h1>Dashboard Page</h1>
-    </section>
+    </Section>
   </>
 );
 
