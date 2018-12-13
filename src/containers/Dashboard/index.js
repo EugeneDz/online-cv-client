32fcbe4751986ef9e3b0f05521476d3fd13a788e
EugeneDz <e.dzhevadov@gmail.com>
2018-12-13 20:54:51 +0200

    feat(dashboard): add spring animation

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { Spring } from 'react-spring';
 
 import { Section } from './styled-components';
 
@@ -9,9 +10,13 @@ const Dashboard = () => (
       <title>Online CV | Dashboard</title>
       <meta name="description" content="Online CV for developers" />
     </Helmet>
-    <Section>
-      <h1>Dashboard Page</h1>
-    </Section>
+    <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+      {spring => (
+        <Section style={spring}>
+          <h1>Dashboard Page</h1>
+        </Section>
+      )}
+    </Spring>
   </>
 );
 
