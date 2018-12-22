4dd20acf3af0814d3e7cb97fbf4f480ab4ec7248
EugeneDz <e.dzhevadov@gmail.com>
2018-12-22 11:50:32 +0200

    feat(dashboard): add profiles layout

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,7 @@ export const USERS_LOGIN = '/api/users/login';
 
 // Profile
 export const CURRENT_PROFILE = '/api/profile';
+export const PROFILE_GET_ALL = '/api/profile/all';
 export const PROFILE_ADD_EDUCATION = '/api/profile/education';
 export const PROFILE_DELETE_EDUCATION = '/api/profile/education/';
 export const PROFILE_ADD_EXPERIENCE = '/api/profile/experience';
--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,67 +1,163 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
-import { Card, Avatar, Tag, Button, Icon, Divider, Row, Col } from 'antd';
+import { Skeleton, Modal, Card, Avatar, Tag, Button, Icon, Divider, Row, Col } from 'antd';
 import { Spring } from 'react-spring';
+
+import { API_URL, PROFILE_GET_ALL } from 'config';
 
 import { Section } from './styled-components';
 
 const { Group: ButtonGroup } = Button;
 
-const Dashboard = () => (
-  <>
-    <Helmet>
-      <title>Online CV | Dashboard</title>
-      <meta name="description" content="Online CV for developers" />
-    </Helmet>
-    <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-      {spring => (
-        <Section style={spring}>
-          <Row>
-            <Col span={24}>
-              <h1>Developer Profiles</h1>
-              <p>
-                Browse profiles or <Link to="/profile">create your own</Link>
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <Divider orientation="right" />
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <Card
-                title={<Avatar size={64} src="" />}
-                extra={
-                  <ButtonGroup>
-                    <Button type="primary">
-                      <Link to={`/profile/${''}`}>
-                        <Icon type="user" /> View Profile
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      noprofiles: '',
+      profiles: []
+    };
+  }
+
+  componentDidMount = () => {
+    this.fetchProfiles();
+  };
+
+  fetchProfiles = async () => {
+    try {
+      const options = {
+        method: 'GET'
+      };
+      const res = await fetch(`${API_URL}${PROFILE_GET_ALL}`, options);
+      const data = await res.json();
+      const status = await res.status;
+
+      // Add promise delay to prevent UI blinking when the response does to fast.
+      await new Promise(resolve => setTimeout(resolve, 400));
+
+      if (status === 404 && data.profile) {
+        this.toggleLoading();
+        this.setState({ noprofiles: data.profile });
+      } else {
+        this.toggleLoading();
+        this.setState({ profiles: data });
+      }
+    } catch (err) {
+      this.onError();
+      this.toggleLoading();
+
+      // Log the error to an error reporting service
+      console.log(err);
+    }
+  };
+
+  toggleLoading = () => {
+    const { loading } = this.state;
+
+    this.setState({ loading: !loading });
+  };
+
+  onError = () =>
+    Modal.error({
+      title: 'Oops, it seems an error occurred!',
+      content: 'We are working on solving the issue.'
+    });
+
+  render() {
+    const { noprofiles, profiles, loading } = this.state;
+    return (
+      <>
+        <Helmet>
+          <title>Online CV | Dashboard</title>
+          <meta name="description" content="Online CV for developers" />
+        </Helmet>
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+          {spring => (
+            <Section style={spring}>
+              <Row>
+                <Col span={24}>
+                  <h1>Developer Profiles</h1>
+                  <p>
+                    Browse profiles or <Link to="/profile"> create your own </Link>
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <Divider orientation="right" />
+                </Col>
+              </Row>
+              {loading ? (
+                <Skeleton avatar active paragraph={{ rows: 4 }} />
+              ) : (
+                <>
+                  {noprofiles && (
+                    <p>
+                      {noprofiles}
+                      <Link to="/profile">
+                        <Divider type="vertical" />
+                        Create your own
                       </Link>
-                    </Button>
-                  </ButtonGroup>
-                }
-                style={{ width: '100%' }}
-              >
-                <Row>
-                  <Col>Name, Status</Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Tag color="geekblue">Skills</Tag>
-                    <Tag color="geekblue">Skills</Tag>
-                    <Tag color="geekblue">Skills</Tag>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
-          </Row>
-        </Section>
-      )}
-    </Spring>
-  </>
-);
+                    </p>
+                  )}
+                  {profiles.length > 0 &&
+                    profiles.map(profile => (
+                      <Row key={profile._id}>
+                        <Col span={24}>
+                          <Card
+                            title={<Avatar size={64} src={profile.user.avatar} />}
+                            extra={
+                              <ButtonGroup>
+                                <Button type="primary">
+                                  <Link to={`/profile/${profile.user._id}`}>
+                                    <Icon type="user" /> View Profile
+                                  </Link>
+                                </Button>
+                              </ButtonGroup>
+                            }
+                            style={{ width: '100%' }}
+                          >
+                            <Row>
+                              <Col span={24}>
+                                {profile.user.name}
+                                <Divider type="vertical" />
+                                {profile.status}
+                              </Col>
+                              <Col span={24}>
+                                <Divider orientation="right" />
+                              </Col>
+                              {profile.bio && (
+                                <Col span={24}>
+                                  <p>{profile.bio}</p>
+                                </Col>
+                              )}
+                            </Row>
+                            <Row>
+                              <Col span={24}>
+                                <Divider orientation="right" />
+                              </Col>
+                              <Col span={24}>
+                                {profile.skills.map(skill => (
+                                  <Tag key={skill} color="geekblue">
+                                    {skill}
+                                  </Tag>
+                                ))}
+                              </Col>
+                            </Row>
+                          </Card>
+                        </Col>
+                      </Row>
+                    ))}
+                </>
+              )}
+            </Section>
+          )}
+        </Spring>
+      </>
+    );
+  }
+}
 
 export default Dashboard;
